45b98c50a7de66ca415ee73b8e2910d82a163b9c
natsugiharto <nathanael.sugiharto@is-gs.com>
2025-11-28 19:12:03 +0700

    added failed status for failed ingestion

--- extraction/routes.py
+++ extraction/routes.py
@@ -31,43 +31,48 @@ class PDFRoutes:
         ):
             print("Processing...")
             await self.repository.update_document_status("processing", int(id))
-            text = await self.handler.extract_text(file, category)
-            
-            # proses ingestion
-            processed_chunks = await self.processor.process_text(
-                text,
-                doc_metadata={"file_id": id, "category": category, "filename": filename}
-            )
 
-            all_docs = []
-            for chunk_data in processed_chunks:
-                chunk_meta = chunk_data.copy()
-                chunk_meta.pop("text", None)
-                # chunk_meta.pop("description", None)
-                docs = parse_chunk_text(chunk_data["text"], default_metadata=chunk_meta)
+            try:
+                text = await self.handler.extract_text(file, category)
+                
+                # proses ingestion
+                processed_chunks = await self.processor.process_text(
+                    text,
+                    doc_metadata={"file_id": id, "category": category, "filename": filename}
+                )
 
-                # chunk_text = (
-                #     f"Document Title: {chunk_data.get('filename','')}\n"
-                #     f"Document Topic: {chunk_data.get('topic','')}\n"
-                #     f"Chunk_Description: {chunk_data.get('description','')}\n"
-                #     f"{chunk_data.get('text','')}"
-                # )
-                # docs = parse_chunk_text(chunk_text, default_metadata=chunk_meta)
-                all_docs.extend(docs)
-            
-            # qdrant
-            upsert_documents(all_docs)
+                all_docs = []
+                for chunk_data in processed_chunks:
+                    chunk_meta = chunk_data.copy()
+                    chunk_meta.pop("text", None)
+                    docs = parse_chunk_text(chunk_data["text"], default_metadata=chunk_meta)
+                    all_docs.extend(docs)
+                
+                # qdrant
+                upsert_documents(all_docs)
 
-            await self.repository.update_document_status("finished", int(id))
+                await self.repository.update_document_status("finished", int(id))
 
-            return {
-                "data": {
-                    "id": id,
-                    "category": category,
-                    "filename": filename,
-                    "chunks_upserted": len(all_docs)
+                return {
+                    "data": {
+                        "id": id,
+                        "category": category,
+                        "filename": filename,
+                        "chunks_upserted": len(all_docs)
+                    }
                 }
-            }
+            except Exception as e:
+                await self.repository.update_document_status("failed", int(id))
+                return {
+                    "data": {
+                        "id": id,
+                        "category": category,
+                        "filename": filename,
+                        "error": str(e)
+                    }
+                }
+            finally:
+                print("extract pdf completed (success or error)")
         
         @self.router.post("/excel")
         async def extract_excel(
@@ -110,15 +115,6 @@ class PDFRoutes:
                 }
             }
 
-
-            # result = {
-            #     "id": id,
-            #     "category": category,
-            #     "filename": filename,
-            #     "extracted_text": text
-            # }
-            # return {"data": result}
-
         @self.router.post("/txt")
         async def extract_txt(
             id: str = Form(...),
@@ -128,56 +124,60 @@ class PDFRoutes:
             key_checked: str = Depends(verify_api_key)
         ):
             await self.repository.update_document_status("processing", int(id))
-            content = await file.read()
-            text = content.decode("utf-8").strip()
 
-            base_metadata = {
-                "file_id": id,
-                "category": category,
-                "filename": filename
-            }
+            try:
+                content = await file.read()
+                text = content.decode("utf-8").strip()
 
-            has_delimiter = "---text---" in text
-            has_faq_pattern = bool(re.search(r"(?mi)^\s*q\s*[:\-]", text))
-
-            all_docs = []
-
-          
-            if has_delimiter or has_faq_pattern:
-                docs = parse_chunk_text(text, default_metadata=base_metadata)
-                # docs = parse_chunk_text(chunk_data["text"], default_metadata=chunk_meta)
-                all_docs.extend(docs)
-            else:
-                processed_chunks = await self.processor.process_text(
-                    text,
-                    doc_metadata=base_metadata
-                )
-
-                for chunk_data in processed_chunks:
-                    chunk_meta = chunk_data.copy()
-                    chunk_meta.pop("text", None)
-                    # chunk_meta.pop("description", None)
-
-                    # chunk_text = (
-                    #     f"Document Title: {chunk_data.get('filename','')}\n"
-                    #     f"Document Topic: {chunk_data.get('topic','')}\n"
-                    #     f"Chunk_Description: {chunk_data.get('description','')}\n"
-                    #     f"{chunk_data.get('text','')}"
-                    # )
-
-                    docs = parse_chunk_text(chunk_data["text"], default_metadata=chunk_meta)
-                    all_docs.extend(docs)
-
-            # --- Upsert ke Qdrant ---
-            upsert_documents(all_docs)
-
-            await self.repository.update_document_status("finished", int(id))
-
-            return {
-                "data": {
-                    "id": id,
+                base_metadata = {
+                    "file_id": id,
                     "category": category,
-                    "filename": filename,
-                    "chunks_upserted": len(all_docs)
+                    "filename": filename
                 }
-            }
+
+                has_delimiter = "---text---" in text
+                has_faq_pattern = bool(re.search(r"(?mi)^\s*q\s*[:\-]", text))
+
+                all_docs = []
+
+            
+                if has_delimiter or has_faq_pattern:
+                    docs = parse_chunk_text(text, default_metadata=base_metadata)
+                    all_docs.extend(docs)
+                else:
+                    processed_chunks = await self.processor.process_text(
+                        text,
+                        doc_metadata=base_metadata
+                    )
+
+                    for chunk_data in processed_chunks:
+                        chunk_meta = chunk_data.copy()
+                        chunk_meta.pop("text", None)
+                        docs = parse_chunk_text(chunk_data["text"], default_metadata=chunk_meta)
+                        all_docs.extend(docs)
+
+                # --- Upsert ke Qdrant ---
+                upsert_documents(all_docs)
+
+                await self.repository.update_document_status("finished", int(id))
+
+                return {
+                    "data": {
+                        "id": id,
+                        "category": category,
+                        "filename": filename,
+                        "chunks_upserted": len(all_docs)
+                    }
+                }
+            except Exception as e:
+                await self.repository.update_document_status("failed", int(id))
+                return {
+                    "data": {
+                        "id": id,
+                        "category": category,
+                        "filename": filename,
+                        "error": str(e)
+                    }
+                }
+            finally:
+                print("extract txt completed (success or error)")
